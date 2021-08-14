7e328640e04db42ced40139441945465993c470f
JoeAda <88879050+JoeAda@users.noreply.github.com>
2021-08-14 17:32:36 +0900

    更新 pxt.json, main.blocks, main.ts, main.json

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > 1) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    } else {
+        motor.motorStopAll()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    motor.motorStopAll()
+})
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    `)
+radio.setGroup(255)
 basic.forever(function () {
 	
 })

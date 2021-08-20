d3d1c4aa512f00bb70997a4149328248fd508222
JoeAda <88879050+JoeAda@users.noreply.github.com>
2021-08-20 14:22:01 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,19 +6,46 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+    if (Motor_A_Status) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+        Motor_A_Status = 0
+    } else {
+        motor.motorStop(motor.Motors.M1)
+        Motor_A_Status = 1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Motor_C_Status) {
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 100)
+        Motor_C_Status = 0
+    } else {
+        motor.motorStop(motor.Motors.M1)
+        Motor_C_Status = 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    motor.motorStopAll()
+    if (Motor_B_Status) {
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
+        Motor_B_Status = 0
+    } else {
+        motor.motorStop(motor.Motors.M2)
+        Motor_B_Status = 1
+    }
 })
+let Motor_C_Status = 0
+let Motor_B_Status = 0
+let Motor_A_Status = 0
 basic.showLeds(`
     # . . . .
     # . . . .
     # . . . .
     # . . . .
-    . . . . .
+    # . . . .
     `)
 radio.setGroup(255)
+Motor_A_Status = 1
+Motor_B_Status = 1
+Motor_C_Status = 1
 basic.forever(function () {
 	
 })

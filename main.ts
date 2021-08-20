bc63f1215c62047d5f7cc6cc0d727c5a74ac2160
JoeAda <88879050+JoeAda@users.noreply.github.com>
2021-08-20 15:56:26 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,48 +9,24 @@ radio.onReceivedNumber(function (receivedNumber) {
             # . . . #
             `)
     } else if (receivedNumber == 1) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
+        basic.showArrow(ArrowNames.North)
     } else if (receivedNumber == 2) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 100)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 100)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . .
-            . # # # .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 70)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 70)
+        basic.showArrow(ArrowNames.South)
     } else if (receivedNumber == 3) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 100)
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 70)
+        basic.showArrow(ArrowNames.East)
     } else if (receivedNumber == 4) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 100)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 70)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
+        basic.showArrow(ArrowNames.West)
     } else if (receivedNumber == 10) {
         if (Motor_C_Status) {
-            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 100)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 70)
             Motor_C_Status = 0
         } else {
             motor.motorStop(motor.Motors.M3)
@@ -62,7 +38,7 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     if (Motor_A_Status) {
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
         Motor_A_Status = 0
     } else {
         motor.motorStop(motor.Motors.M2)
@@ -71,7 +47,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     if (Motor_C_Status) {
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 100)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 70)
         Motor_C_Status = 0
     } else {
         motor.motorStop(motor.Motors.M3)
@@ -80,7 +56,7 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (Motor_B_Status) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 100)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
         Motor_B_Status = 0
     } else {
         motor.motorStop(motor.Motors.M1)
@@ -90,13 +66,7 @@ input.onButtonPressed(Button.B, function () {
 let Motor_C_Status = 0
 let Motor_B_Status = 0
 let Motor_A_Status = 0
-basic.showLeds(`
-    # . . . .
-    # . . . .
-    # . . . .
-    # . . . .
-    # . . . .
-    `)
+basic.showString("Motor")
 radio.setGroup(255)
 Motor_A_Status = 1
 Motor_B_Status = 1

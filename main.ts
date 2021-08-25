6c7a97133ad0cc045b10c6894d84d00fd312b143
JoeAda <88879050+JoeAda@users.noreply.github.com>
2021-08-25 14:39:41 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,24 +9,24 @@ radio.onReceivedNumber(function (receivedNumber) {
             # # . # .
             `)
     } else if (receivedNumber == 1) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, MaxSpeed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, MaxSpeed)
         basic.showArrow(ArrowNames.North)
     } else if (receivedNumber == 2) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 70)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 70)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, MaxSpeed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, MaxSpeed)
         basic.showArrow(ArrowNames.South)
     } else if (receivedNumber == 3) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 70)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, MaxSpeed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, MaxSpeed)
         basic.showArrow(ArrowNames.East)
     } else if (receivedNumber == 4) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 70)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, MaxSpeed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, MaxSpeed)
         basic.showArrow(ArrowNames.West)
     } else if (receivedNumber == 10) {
         if (Motor_C_Status) {
-            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 70)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, MaxSpeed)
             Motor_C_Status = 0
         } else {
             motor.motorStop(motor.Motors.M3)
@@ -38,7 +38,7 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     if (Motor_A_Status) {
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, MaxSpeed)
         Motor_A_Status = 0
     } else {
         motor.motorStop(motor.Motors.M2)
@@ -56,13 +56,14 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (Motor_B_Status) {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 70)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, MaxSpeed)
         Motor_B_Status = 0
     } else {
         motor.motorStop(motor.Motors.M1)
         Motor_B_Status = 1
     }
 })
+let MaxSpeed = 0
 let Motor_C_Status = 0
 let Motor_B_Status = 0
 let Motor_A_Status = 0
@@ -71,6 +72,7 @@ radio.setGroup(255)
 Motor_A_Status = 1
 Motor_B_Status = 1
 Motor_C_Status = 1
+MaxSpeed = 255
 basic.forever(function () {
 	
 })
